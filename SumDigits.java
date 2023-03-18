67726748dd4947d1fdfab155267f37631551e88d
Gaurav-Zore <128240011+Gaurav-Zore@users.noreply.github.com>
2023-03-18 21:00:09 +0530

    Add files via upload

---
+++ SumDigits.java
@@ -0,0 +1,13 @@
+public class SumDigits {
+    
+    public static void main(String[] args) {
+        int num = 123;
+        int sum = 0;
+
+        while(num != 0){
+            sum = sum + num % 10;
+            num/=10;
+        }
+        System.out.println(sum);
+    }
+}
