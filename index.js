cade1d2ae7640218ceeb8d35a6ccf50329a7bc5b
Gaurav-Zore <128240011+Gaurav-Zore@users.noreply.github.com>
2023-03-24 01:37:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Divisible By 3
+
+var arr = [95,85,76,25,45,36,18,72,54];
+
+function divByThree(){
+for(let i=0; i<arr.length; i++){
+    if(arr[i]%3==0){
+        console.log(arr[i]);
+    }
+}
+}
+divByThree();
+
